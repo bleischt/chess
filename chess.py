d88093791a50bca07ac1f21eebab83bbe3cc1593
Tobias Bleisch <bleischt@hotmail.com>
2018-04-30 21:52:47 -0700

    simple chess board and print function

---
+++ chess.py
@@ -0,0 +1,14 @@
+
+
+width = 8
+height = 8
+board = [['O' for row in range(width)] for col in range(height)]
+
+def printBoard(board):
+    for row in board:
+        print(row)
+
+printBoard(board)
+
+
+
